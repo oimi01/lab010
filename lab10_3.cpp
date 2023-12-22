b406c406c48c7efeacb92dc9feace61c61c0c568
kirakorn <apikorn.111@gamil.com>
2023-12-22 13:12:45 +0700

    complete

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,26 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+ ifstream source;
+ ofstream dest;
+ source.open("cheerbook.txt") ;
+ dest.open("cheerbook_copy.txt");
+ 
+ string textline;
+ dest << "-------------------- BOOM ---------------------\n";
+ for(int i = 0;i < 9 ; i++ ){
+  getline(source,textline);
+  dest << textline<<endl;
+ }
+ dest  << "-------------------- HA!! ---------------------";
+
+    source.close();
+    dest.close();
+ return 0;
+}
